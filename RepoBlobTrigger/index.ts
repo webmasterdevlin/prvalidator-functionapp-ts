13770a8de257f68685fc3db0127fc3bc87dd13e3
Devlin D <webmasterdevlin>
2020-06-16 15:33:19 +0200

    removed context log blob

--- RepoBlobTrigger/index.ts
+++ RepoBlobTrigger/index.ts
@@ -1,8 +1,16 @@
-import { AzureFunction, Context } from "@azure/functions"
+import { AzureFunction, Context } from "@azure/functions";
 
-const blobTrigger: AzureFunction = async function (context: Context, myBlob: any): Promise<void> {
-    context.log("myBlob::", myBlob);
-    context.log("Blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
+const blobTrigger: AzureFunction = async function (
+  context: Context,
+  myBlob: any
+): Promise<void> {
+  context.log(
+    "Blob trigger function processed blob \n Name:",
+    context.bindingData.name,
+    "\n Blob Size:",
+    myBlob.length,
+    "Bytes"
+  );
 };
 
 export default blobTrigger;
