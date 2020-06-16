96b461540e31231f015bde8aa3d29e4004157a4b
Devlin D <webmasterdevlin>
2020-06-17 00:31:48 +0200

    update blob

--- RepoBlobTrigger/index.ts
+++ RepoBlobTrigger/index.ts
@@ -6,12 +6,14 @@ import {
 } from "@azure/storage-blob";
 import { AbortController } from "@azure/abort-controller";
 import { Description, State, StatusPolicy } from "../models/StatusPolicy";
+import { PullRequestCreated } from "../models/webhooks/PullRequestCreated";
 
 const CONNECTION_STRING = process.env.CONNECTION_STRING;
 const CONTAINER_NAME = process.env.REPO_CONTAINER_NAME;
 
 const ONE_MINUTE = 60 * 1000;
 const blobName = "Contributors.md";
+const prBlobName = "pull-request-created.json";
 
 const blobTrigger: AzureFunction = async function (
   context: Context,
@@ -29,6 +31,10 @@ const blobTrigger: AzureFunction = async function (
     blobName
   );
 
+  const prBlockBlobClient: BlockBlobClient = containerClient.getBlockBlobClient(
+    prBlobName
+  );
+
   const exists = await blockBlobClient.exists();
   if (!exists) {
     context.log("Blob does not exists");
@@ -36,28 +42,45 @@ const blobTrigger: AzureFunction = async function (
   }
 
   const downloadTobuffer: Buffer = await blockBlobClient.downloadToBuffer();
-  const text = downloadTobuffer.toString();
+  const md = downloadTobuffer.toString();
 
-  if (!text) {
-    context.log("EMPTY!!");
+  const prExists = await prBlockBlobClient.exists();
+  if (!prExists) {
+    context.log("Blob does not exists");
+    return;
+  }
 
-    const statusPolicy = new StatusPolicy(
-      State.succeeded,
-      Description.succeeded
-    );
+  const prDownloadTobuffer: Buffer = await prBlockBlobClient.downloadToBuffer();
+  const pr = prDownloadTobuffer.toString();
+
+  context.log("PR::", pr);
+
+  const data = JSON.parse(pr) as PullRequestCreated;
+
+  context.log("###################################################");
+
+  context.log("DATA::", data);
+
+  if (!md) {
+    context.log("MD is EMPTY!!");
+
     /*
-    const status = (
-      await updateStatusPolicy(
-        statusPolicy,
-        accountName,
-        prData.resource.repository.project.name,
-        prData.resource.repository.name,
-        prData.resource.pullRequestId,
-        context
-      )
-    ).data;*/
-
-    context.log("STATUS::", status);
+       const statusPolicy = new StatusPolicy(
+         State.succeeded,
+         Description.succeeded
+       );
+       const status = (
+         await updateStatusPolicy(
+           statusPolicy,
+           accountName,
+           prData.resource.repository.project.name,
+           prData.resource.repository.name,
+           prData.resource.pullRequestId,
+           context
+         )
+       ).data;
+       context.log("STATUS::", status);
+        */
   } else {
     context.log("NOT EMPTY!!");
   }
