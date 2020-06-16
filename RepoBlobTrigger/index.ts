2794b158c6db19eb2f349568203a123fb6010410
Devlin D <webmasterdevlin>
2020-06-16 22:11:03 +0200

    update blob

--- RepoBlobTrigger/index.ts
+++ RepoBlobTrigger/index.ts
@@ -24,13 +24,13 @@ const blobTrigger: AzureFunction = async function (
   context: Context,
   myBlob: any
 ): Promise<void> {
-  context.log(
+  /*  context.log(
     "Blob trigger function processed blob \n Name:",
     context.bindingData.name,
     "\n Blob Size:",
     myBlob.length,
     "Bytes"
-  );
+  );*/
 
   const abortCtrl = AbortController.timeout(5 * ONE_MINUTE);
 
