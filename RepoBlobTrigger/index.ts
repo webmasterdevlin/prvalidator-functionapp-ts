65ffd719fedeb7d3df986df97ce0a77933202d7e
Devlin D <webmasterdevlin>
2020-06-16 20:28:17 +0200

    update blob

--- RepoBlobTrigger/index.ts
+++ RepoBlobTrigger/index.ts
@@ -2,19 +2,65 @@ import { AzureFunction, Context } from "@azure/functions";
 import {
   StorageSharedKeyCredential,
   BlobServiceClient,
+  ContainerClient,
+  BlobClient,
+  BlockBlobClient,
+  BlobDownloadResponseModel,
 } from "@azure/storage-blob";
+import { AbortController } from "@azure/abort-controller";
+
+const fs = require("fs");
+const path = require("path");
 
 const STORAGE_ACCOUNT_NAME = process.env.ACCOUNT;
-const ACCOUNT_ACCESS_KEY = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
+const ACCOUNT_ACCESS_KEY = process.env.ACCESS_KEY;
+const CONNECTION_STRING = process.env.CONNECTION_STRING;
+const CONTAINER_NAME = process.env.REPO_CONTAINER_NAME;
 
-const ONE_MEGABYTE = 1024 * 1024;
-const FOUR_MEGABYTES = 4 * ONE_MEGABYTE;
 const ONE_MINUTE = 60 * 1000;
 
 const blobTrigger: AzureFunction = async function (
   context: Context,
   myBlob: any
 ): Promise<void> {
+  const abortCtrl = AbortController.timeout(5 * ONE_MINUTE);
+
+  const blobName = "Contributors.md";
+  const blobServiceClient = await BlobServiceClient.fromConnectionString(
+    CONNECTION_STRING
+  );
+  const containerClient: ContainerClient = await blobServiceClient.getContainerClient(
+    CONTAINER_NAME
+  );
+  const blockBlobClient: BlockBlobClient = containerClient.getBlockBlobClient(
+    blobName
+  );
+
+  const exists = await blockBlobClient.exists();
+  if (!exists) {
+    console.log("Blob does not exists");
+    return;
+  }
+
+  const downloadBlockBlobResponse: BlobDownloadResponseModel = await blockBlobClient.download(
+    0
+  );
+
+  // TODO: Check if readabeStreamBody is needed or downloadBlockBlobResponse
+  const readable = downloadBlockBlobResponse.readableStreamBody;
+
+  try {
+    const data = await fs.readFile(readable);
+    context.log(data);
+    if (!data) {
+      context.log("File is empty");
+    } else {
+      context.log("File not empty");
+    }
+  } catch (error) {
+    context.log(error);
+  }
+
   context.log(
     "Blob trigger function processed blob \n Name:",
     context.bindingData.name,
@@ -25,76 +71,3 @@ const blobTrigger: AzureFunction = async function (
 };
 
 export default blobTrigger;
-
-// [Node.js only] A helper method used to read a Node.js readable stream into string
-async function streamToString(readableStream) {
-  return new Promise((resolve, reject) => {
-    const chunks = [];
-    readableStream.on("data", (data) => {
-      chunks.push(data.toString());
-    });
-    readableStream.on("end", () => {
-      resolve(chunks.join(""));
-    });
-    readableStream.on("error", reject);
-  });
-}
-
-async function execute() {
-  const containerName = "demo";
-  const blobName = "quickstart.txt";
-  const content = "Hello Node SDK";
-  const localFilePath = "../README.md";
-
-  const credentials = new StorageSharedKeyCredential(
-    STORAGE_ACCOUNT_NAME,
-    ACCOUNT_ACCESS_KEY
-  );
-
-  const blobServiceClient = new BlobServiceClient(
-    `https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net`,
-    credentials
-  );
-
-  const containerClient = blobServiceClient.getContainerClient(containerName);
-  const blobClient = containerClient.getBlobClient(blobName);
-  const blockBlobClient = blobClient.getBlockBlobClient();
-
-  const aborter = AbortController.timeout(30 * ONE_MINUTE);
-
-  await containerClient.create();
-  console.log(`Container: "${containerName}" is created`);
-
-  console.log("Containers:");
-  await showContainerNames(aborter, blobServiceClient);
-
-  await blockBlobClient.upload(content, content.length, aborter);
-  console.log(`Blob "${blobName}" is uploaded`);
-
-  await uploadLocalFile(aborter, containerClient, localFilePath);
-  console.log(`Local file "${localFilePath}" is uploaded`);
-
-  await uploadStream(aborter, containerClient, localFilePath);
-  console.log(`Local file "${localFilePath}" is uploaded as a stream`);
-
-  console.log(`Blobs in "${containerName}" container:`);
-
-  await showBlobNames(aborter, containerClient);
-
-  const downloadResponse = await blockBlobClient.download(0, aborter);
-  const downloadedContent = await streamToString(
-    downloadResponse.readableStreamBody
-  );
-
-  console.log(`Downloaded blob content: "${downloadedContent}"`);
-
-  await blockBlobClient.delete(aborter);
-  console.log(`Block blob "${blobName}" is deleted`);
-
-  await containerClient.delete(aborter);
-  console.log(`Container "${containerName}" is deleted`);
-}
-
-execute()
-  .then(() => console.log("Done"))
-  .catch((e) => console.log(e));
