428a44e88750b22063ca16a576307cda24f2157c
Devlin D <webmasterdevlin>
2020-06-16 23:00:43 +0200

    update blob

--- RepoBlobTrigger/index.ts
+++ RepoBlobTrigger/index.ts
@@ -8,6 +8,8 @@ import {
   BlobDownloadResponseModel,
 } from "@azure/storage-blob";
 import { AbortController } from "@azure/abort-controller";
+import { Description, State, StatusPolicy } from "../models/StatusPolicy";
+import { updateStatusPolicy } from "../api-calls";
 
 const fs = require("fs");
 const path = require("path");
@@ -16,6 +18,8 @@ const STORAGE_ACCOUNT_NAME = process.env.ACCOUNT;
 const ACCOUNT_ACCESS_KEY = process.env.ACCESS_KEY;
 const CONNECTION_STRING = process.env.CONNECTION_STRING;
 const CONTAINER_NAME = process.env.REPO_CONTAINER_NAME;
+
+const accountName = process.env.ACCOUNT_NAME;
 
 const ONE_MINUTE = 60 * 1000;
 const blobName = "Contributors.md";
@@ -49,6 +53,27 @@ const blobTrigger: AzureFunction = async function (
 
   if (!text) {
     context.log("EMPTY!!");
+
+    const statusPolicy = new StatusPolicy(
+      State.succeeded,
+      Description.succeeded
+    );
+    /*    context.log(
+      `https://dev.azure.com/${accountName}/${prData.resource.repository.project.name}/_apis/git/repositories/${prData.resource.repository.name}/pullrequests/${prData.resource.pullRequestId}/statuses?api-version=5.0-preview.1`
+    );
+
+    const status = (
+      await updateStatusPolicy(
+        statusPolicy,
+        accountName,
+        prData.resource.repository.project.name,
+        prData.resource.repository.name,
+        prData.resource.pullRequestId,
+        context
+      )
+    ).data;*/
+
+    context.log("STATUS::", status);
   } else {
     context.log("NOT EMPTY!!");
   }
